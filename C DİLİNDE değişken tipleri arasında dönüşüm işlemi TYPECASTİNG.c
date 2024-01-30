65da4d4f3a66c5e79a11a12826efa43c6f58236e
Barış Özsefil <barisozsefil@gmail.com>
2024-01-30 13:09:49 +0300

    f
    f

---
+++ C DİLİNDE değişken tipleri arasında dönüşüm işlemi TYPECASTİNG.c
@@ -0,0 +1,26 @@
+//*C DİLİNDE değişken tipleri arasında dönüşüm işlemi "TYPECASTİNG"* //
+#include <stdio.h>
+
+int main(){
+     int sayi1 =17 ,sayi2 =5;
+     
+     int tamsayi_bolme_sonucu;
+     float gercel_bolme_sonucu_float;
+     double gercel_bolme_sonucu_double;
+     int gercel_bolme_sonucu_float_tamsayi;
+     
+     tamsayi_bolme_sonucu = sayi1 / sayi2;
+     printf("Tamsayı Bölme Sonucu %d\n", tamsayi_bolme_sonucu);
+     
+     gercel_bolme_sonucu_float = (float) sayi1/sayi2;
+     printf("Gerçek bölme sonucu (float) =%f\n", gercel_bolme_sonucu_float);
+     
+     gercel_bolme_sonucu_double = (double) sayi1/sayi2;
+     printf("Gerçek bölme sonucu (double) %f\n" ,gercel_bolme_sonucu_double);
+     
+     gercel_bolme_sonucu_float_tamsayi = (int) gercel_bolme_sonucu_float ;
+     printf("Gerçek bölme sonucu (float) un Tam sayıya dönüştürülmüş hali %d\n",gercel_bolme_sonucu_float_tamsayi);
+     
+    return 0; 
+}
+
---
+++ ders.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 3 ;
+    printf ("%d\n",i);
+    
+    i += 5;             //i = i + 5 
+    printf("%d\n",i);
+    
+    i-=9;              //i = i - 9
+    printf("%d\n",i);
+
+    
+    return 0;
+}
